585b1a17cbf8c96ec3ec189a7a32bfef83a774ac
Mohammad Abdelkader Omar <34446061+mabdulkadr@users.noreply.github.com>
2026-08-31 08:54:03 +0300

    Delete Install Sysmon with Config File directory

--- Install Sysmon with Config File/SysmonInstall.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-REM Change directory to the location of the script
-cd /d "%~dp0"
-
-REM Install Sysmon with the specified configuration file silently
-Sysmon64.exe -accepteula -i sysmonconfig.xml 
-
-exit
